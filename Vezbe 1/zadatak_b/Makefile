4be4705785c633702edb6b8b58ffa1dd9e5a1345
vesnaaaaaa <jankovicvesna04@gmail.com>
2024-12-26 15:49:31 +0100

    Zadatak za vezbu pod b

---
+++ Vezbe 1/zadatak_b/Makefile
@@ -0,0 +1,9 @@
+all: zadatak
+build: zadatak.c
+	gcc -c zadatak.c
+link: zadatak.o
+	gcc -o zadatak zadatak.o
+clean: 
+	rm zadatak.o zadatak
+run: zadatak
+	./zadatak
---
+++ Vezbe 1/zadatak_b/zadatak_b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int x=10;
+int y=2;
+int rezultat;
+
+void main(){
+	
+	if(y==0){
+		
+		printf("Nije dozvoljeno deljenje sa nulom! \n", y);	
+	}
+	else
+	{
+		
+		rezultat= x/y;
+		printf("Rezultat je jednak broju: %d /n");
+	}
+	
+	
+	
+	
+}
